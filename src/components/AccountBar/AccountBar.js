c5f4cf6627c1b340098611264630231d92deb6ba
Trevor Leake <contact@trevorleake.com>
2019-05-11 11:50:05 -0700

    Added Kaggle logo & resized others for uniformity.

--- src/components/AccountBar/AccountBar.js
+++ src/components/AccountBar/AccountBar.js
@@ -10,13 +10,56 @@ import { ReactComponent as LinkedIn } from '../../static/logos/linkedin.svg'
 import { ReactComponent as Soundcloud } from '../../static/logos/soundcloud.svg'
 import { ReactComponent as Twitter } from '../../static/logos/twitter.svg'
 import { ReactComponent as StackExchange } from '../../static/logos/stack-exchange.svg'
+import { ReactComponent as Kaggle } from '../../static/logos/kaggle.svg'
+
 
 class AccountBar extends Component {
   render() {
-    const onColor = "#ffffff"
-    const offColor = "#000000"
+
+    let logos = [
+      "Github",
+      "Bitbucket",
+      "Kaggle",
+      "StackExchange",
+      "Twitter",
+      "Instagram",
+      "Medium",
+      "ItchIO",
+      "LinkedIn",
+      "Soundcloud"
+    ]
+
+    const onColor = "#fff"
+    const offColor = "#000"
     const width = "auto"
     const height = "40px"
+
+    const nameToSVG = {
+      "Github":         Github,
+      "Bitbucket":      Bitbucket,
+      "Instagram":      Instagram,
+      "Medium":         Medium,
+      "ItchIO":         ItchIO,
+      "LinkedIn":       LinkedIn,
+      "Soundcloud":     Soundcloud,
+      "StackExchange":  StackExchange,
+      "Twitter":        Twitter,
+      "Kaggle":         Kaggle
+    }
+
+    const nameToLink = {
+      "Github": "https://www.github.com/trevorleake",
+      "Bitbucket": "https://www.bitbucket.com/trevorleake",
+      "Instagram": "https://www.instagram.com/trevorjohnleake",
+      "Medium": "https://medium.com/@trevorjohnleake",
+      "ItchIO": "https://trevorleake.itch.io/",
+      "LinkedIn": "https://www.linkedin.com/in/trevor-leake-8b4819132/",
+      "Soundcloud": "https://www.soundcloud.com/trevor_leake",
+      "StackExchange": "https://stackexchange.com/users/8644667/qfwfq",
+      "Twitter": "https://www.twitter.com/trevorleake",
+      "Kaggle": "https://www.kaggle.com/trevorleake"
+    }
+
 
     let colorOn = e => {e.setAttribute('fill', onColor)}
     let colorOff = e => {e.setAttribute('fill', offColor)}
@@ -32,46 +75,27 @@ class AccountBar extends Component {
         colorFunction(element);
     }
 
-
-
     return (
       <div className="account-bar">
-
-        <a href="https://www.github.com/trevorleake">
-          <Github width={width} height={height} onMouseEnter={e => deepMap(e.currentTarget, colorOn)} onMouseLeave={e => deepMap(e.currentTarget, colorOff)} />
-        </a>
-
-        <a href="https://www.bitbucket.com/trevorleake">
-          <Bitbucket width={width} height={height} onMouseEnter={e => deepMap(e.currentTarget, colorOn)} onMouseLeave={e => deepMap(e.currentTarget, colorOff)} />
-        </a>
-
-        <a href="https://www.instagram.com/trevorleake">
-          <Instagram width={width} height={height} onMouseEnter={e => deepMap(e.currentTarget, colorOn)} onMouseLeave={e => deepMap(e.currentTarget, colorOff)} />
-        </a>
-
-        <a href="https://www.medium.com">
-          <Medium width={width} height={height} onMouseEnter={e => deepMap(e.currentTarget, colorOn)} onMouseLeave={e => deepMap(e.currentTarget, colorOff)} />
-        </a>
-
-        <a href="https://www.itchio.com">
-          <ItchIO width={width} height={height} onMouseEnter={e => deepMap(e.currentTarget, colorOn)} onMouseLeave={e => deepMap(e.currentTarget, colorOff)} />
-        </a>
-
-        <a href="https://www.linkedin.com">
-          <LinkedIn width={width} height={height} onMouseEnter={e => deepMap(e.currentTarget, colorOn)} onMouseLeave={e => deepMap(e.currentTarget, colorOff)} />
-        </a>
-
-        <a href="https://www.soundcloud.com">
-          <Soundcloud width={width} height={height} onMouseEnter={e => deepMap(e.currentTarget, colorOn)} onMouseLeave={e => deepMap(e.currentTarget, colorOff)} />
-        </a>
-
-        <a href="https://www.stackexchange.com">
-          <StackExchange width={width} height={height} onMouseEnter={e => deepMap(e.currentTarget, colorOn)} onMouseLeave={e => deepMap(e.currentTarget, colorOff)} />
-        </a>
-
-        <a href="https://www.twitter.com/trevorleake">
-          <Twitter width={width} height={height} onMouseEnter={e => deepMap(e.currentTarget, colorOn)} onMouseLeave={e => deepMap(e.currentTarget, colorOff)} />
-        </a>
+        {
+          logos.map(item => (
+            /* The link surrounds the SVG element */
+            <a href={nameToLink[item]}>
+              {
+                /* Found that it was difficult to render an element with a dynamic className */
+                React.createElement(
+                  nameToSVG[item],
+                  {
+                    width: width,
+                    height: height,
+                    onMouseEnter: e => deepMap(e.currentTarget, colorOn),
+                    onMouseLeave: e => deepMap(e.currentTarget, colorOff)
+                  }
+                )
+              }
+            </a>
+          ))
+        }
       </div>
     );
   }
