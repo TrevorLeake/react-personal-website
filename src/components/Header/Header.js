92be76d02aebb24cf189c96f86a0a2d18ef7e08f
Trevor Leake <trevorjohnleake@gmail.com>
2019-06-07 12:18:45 -0700

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import AccountBar from '../AccountBar/AccountBar';
 import './Header.css';
-import bodyImage from '../../static/full-body.svg';
+import bodyImage from '../../static/full-body-technicolor.svg';
 
 class Header extends Component {
   render() {
